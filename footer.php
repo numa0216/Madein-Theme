6394ec9f887296806df9ac80e95a81d2f02e5d79
naganuma-tsutomu <123545778+naganuma-tsutomu@users.noreply.github.com>
2023-06-14 18:58:45 +0900

    update: 更新

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
-<footer>
-  </footer>
-  <?php wp_footer(); ?>
- </body>
+<footer class="footer">
+  <div class="icon"><a href="">1</a></div>
+  <div class="icon"><a href="">2</a></div>
+  <div class="icon"><a href="">3</a></div>
+  <div class="icon"><a href="">4</a></div>
+  <div class="icon"><a href="">5</a></div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
 </html>
