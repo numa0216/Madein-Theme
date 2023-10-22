d551612500bceca6c2cf3a2c9ca076b9a27f505b
naganuma <m2106m@gmail.com>
2023-10-22 21:55:22 +0900

    update

--- src/templates/parts/breadcrumb.php
+++ src/templates/parts/breadcrumb.php
@@ -3,7 +3,7 @@ if (is_front_page()) :
     return;
 else : ?>
     <nav class="nav">
-        <a href="<?php echo esc_url(home_url('/')); ?>" rel="nofollow">HOME</a>
+        <a href="<?php echo esc_url(home_url('/')); ?>" rel="nofollow">Home</a>
         <span>»</span>
         <?php if (is_category() || is_single()) : ?>
             <!-- the_category (' • '); -->
@@ -12,6 +12,8 @@ else : ?>
             <?php the_title(); ?>
         <?php elseif (is_search()) : ?>
             <?php the_title(); ?>
+        <?php elseif (is_post_type_archive('works')) : ?>
+            Works
         <?php endif; ?>
     </nav>
 <?php endif; ?>
