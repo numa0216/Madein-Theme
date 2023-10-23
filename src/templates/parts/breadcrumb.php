a466a99e1baa7ff74109d272c9dfc34bf5e5493d
naganuma <m2106m@gmail.com>
2023-10-23 21:37:07 +0900

    update

--- src/templates/parts/breadcrumb.php
+++ src/templates/parts/breadcrumb.php
@@ -3,17 +3,19 @@ if (is_front_page()) :
     return;
 else : ?>
     <nav class="nav">
-        <a href="<?php echo esc_url(home_url('/')); ?>" rel="nofollow">Home</a>
-        <span>»</span>
-        <?php if (is_category() || is_single()) : ?>
-            <!-- the_category (' • '); -->
-            <?php the_title(); ?>
-        <?php elseif (is_page()) : ?>
-            <?php the_title(); ?>
-        <?php elseif (is_search()) : ?>
-            <?php the_title(); ?>
-        <?php elseif (is_post_type_archive('works')) : ?>
-            Works
-        <?php endif; ?>
+        <div class="nav__wrap">
+            <a href="<?php echo esc_url(home_url('/')); ?>" rel="nofollow">Home</a>
+            <span>»</span>
+            <?php if (is_category() || is_single()) : ?>
+                <!-- the_category (' • '); -->
+                <?php the_title(); ?>
+            <?php elseif (is_page()) : ?>
+                <?php the_title(); ?>
+            <?php elseif (is_search()) : ?>
+                <?php the_title(); ?>
+            <?php elseif (is_post_type_archive('works')) : ?>
+                Works
+            <?php endif; ?>
+        </div>
     </nav>
 <?php endif; ?>
